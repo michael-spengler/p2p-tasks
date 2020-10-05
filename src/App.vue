023ff384b239a02f0ca1e3ac77973daa1647a76d
michael-spengler <mycamper@gmx.de>
2020-10-05 21:10:23 +0200

    learning vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
+
 <template>
   <div id="app">
     <Todos v-bind:todos="todos" />
+    <AddTodo v-on:add-todo="addTodo" />
   </div>
 </template>
-<script>
 
+<script>
 import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo";
+
 export default {
+
   name: "app",
+
   components: {
-    Todos
+    Todos,
+    AddTodo
   },
+
   data() {
     return {
       todos: [
@@ -41,8 +49,16 @@ export default {
         }
       ]
     };
+  },
+
+  methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj];
+    }
   }
+
 };
+
 </script>
 <style>
 </style>
