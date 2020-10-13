346002c2bfbef6b836fe81d879330c5a77b23b15
michael-spengler <mycamper@gmx.de>
2020-10-13 23:46:57 +0200

    deployment

--- serve.bash
+++ serve.bash
@@ -1,1 +1,3 @@
 deno run --allow-net --allow-read --allow-write server/p2p-tasks-server.ts
+
+# pm2 start server/p2p-tasks-server.ts --interpreter="deno" --interpreter-args="run --allow-net --allow-read --allow-write"
